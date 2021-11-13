2f6f60475ec9289f854124cd6424d0df47c6f205
Paul Bowden <paulbowden673@gmail.com>
2021-11-13 14:06:52 +0000

    added index.html

--- app.py
+++ app.py
@@ -30,6 +30,10 @@ def register():
     return render_template("register.html")
 
 
+@app.route("/index", methods=["GET", "POST"])
+def index():
+    return render_template("index.html")
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
